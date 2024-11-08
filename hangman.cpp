8721ec93ffa794015ad59ab5da4ca7aadbcc9931
Vincent <vincentnadzeyka@gmail.com>
2024-11-08 15:31:09 +0100

    finished

--- hangman.cpp
+++ hangman.cpp
@@ -4,46 +4,45 @@
 #include <cstdlib>
 #include <ctime>
 
-
-void displayHangman(int wrongGuesses){
-    switch (wrongGuesses)
-    {
+// displays the hangman figure
+void displayHangman(int wrongGuesses) {
+    switch (wrongGuesses) {
     case 0:
-        std::cout << "   _______     " << std::endl;
-        std::cout << "  |       |    " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << " _|_           " << std::endl << std::endl;
+        std::cout << "   _______     \n";
+        std::cout << "  |       |    \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << " _|_           \n\n";
         break;
 
     case 1:
-        std::cout << "   _______     " << std::endl;
-        std::cout << "  |       |    " << std::endl;
-        std::cout << "  |       O    " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << "  |            " << std::endl;
-        std::cout << " _|_           " << std::endl << std::endl;
+        std::cout << "   _______     \n";
+        std::cout << "  |       |    \n";
+        std::cout << "  |       O    \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << " _|_           \n\n";
         break;
-        
+
     case 2:
-        std::cout << "   _______     \n" << std::endl;
-        std::cout << "  |       |    \n" << std::endl;
-        std::cout << "  |       O    \n" << std::endl;
-        std::cout << "  |      /|    \n" << std::endl;
-        std::cout << "  |            \n" << std::endl;
-        std::cout << "  |            \n" << std::endl;
-        std::cout << " _|_           " << std::endl << std::endl;
+        std::cout << "   _______     \n";
+        std::cout << "  |       |    \n";
+        std::cout << "  |       O    \n";
+        std::cout << "  |      /|    \n";
+        std::cout << "  |            \n";
+        std::cout << "  |            \n";
+        std::cout << " _|_           \n\n";
         break;
 
     case 3:
         std::cout << "   _______     \n";
         std::cout << "  |       |    \n";
         std::cout << "  |       O    \n";
-        std::cout << "  |      /|    \n";
-        std:: cout << "  |            \n";
+        std::cout << "  |      /|\\  \n";
+        std::cout << "  |            \n";
         std::cout << "  |            \n";
         std::cout << " _|_           \n\n";
         break;
@@ -53,7 +52,7 @@ void displayHangman(int wrongGuesses){
         std::cout << "  |       |    \n";
         std::cout << "  |       O    \n";
         std::cout << "  |      /|\\  \n";
-        std::cout << "  |            \n";
+        std::cout << "  |      /     \n";
         std::cout << "  |            \n";
         std::cout << " _|_           \n\n";
         break;
@@ -63,56 +62,75 @@ void displayHangman(int wrongGuesses){
         std::cout << "  |       |    \n";
         std::cout << "  |       O    \n";
         std::cout << "  |      /|\\  \n";
-        std::cout << "  |      /     \n";
+        std::cout << "  |      / \\  \n";
         std::cout << "  |            \n";
         std::cout << " _|_           \n\n";
         break;
-
-    case 6:
-        std::cout << "   _______     ";
-        std::cout << "  |       |    ";
-        std::cout << "  |       O    ";
-        std::cout << "  |      /|\\  ";
-        std::cout << "  |      / \\  ";
-        std::cout << "  |            ";
-        std::cout << " _|_           ";
-        break;
-
-    
     }
 }
 
-void displayWord(const string& word, const vector<bool>& guessed);
-    for(size_t i = 0; i < word.length(); ++i){
-        if(guessed[i]){
+
+// displays state of the word 
+void displayWord(const std::string& word, const std::vector<bool>& guessed) {
+    for (size_t i = 0; i < word.length(); i++) {
+        if (guessed[i]) {
             std::cout << word[i] << " ";
-        } else{
-            cout << "_";
+        } else {
+            std::cout << "_ ";
         }
     }
+    std::cout << std::endl;
+}
+
 int main() {
     const std::vector<std::string> wordList = {"Venustraphobia", "Chromophobia", "Apple", "River", "Feather", "Mirror", "Spark", "Clock", "Sapphire", "Phoenix"};
-    int attempts = 10;
+    srand(time(0));
+    std::string word = wordList[rand() % wordList.size()];  // Selects a word from the list
+    std::vector<bool> guessed(word.length(), false);
+    int wrongGuesses = 0;
+    const int maxGuesses = 6;
 
-    // Select a random word from the list
-    std::string chosenWord = wordList[rand() % wordList.size()];
+    std::cout << "HANGMAN" << std::endl;
 
-    while (attempts > 0) {
-        std::cout << "*****HANGMAN******" << std::endl;
-        std::cout << "Guess the word by entering a letter." << std::endl;
-        std::cout << "You have " << attempts << " attempts left" << std::endl;
-        std::vector<char> guessedWord(chosenWord.length(), '_');
-        std::cin >> chosenWord;
+    while (wrongGuesses < maxGuesses) {
+        displayHangman(wrongGuesses);
+        displayWord(word, guessed);
 
-        for (char c : guessedWord) {
-            std::cout << c << ' ';
+        std::cout << "Enter a letter: ";
+        char guess;
+        std::cin >> guess;
+
+        bool correctGuess = false;
+        for (size_t i = 0; i < word.length(); i++) {
+            if (tolower(word[i]) == tolower(guess) && !guessed[i]) {
+                guessed[i] = true;
+                correctGuess = true;
+            }
         }
-        
 
-        std::cout << "*****************" << std::endl;
+        if (!correctGuess) {
+            wrongGuesses++;
+            std::cout << "Wrong! You've got " << maxGuesses - wrongGuesses << " guesses left." << std::endl;
+        }
+
+        bool wordComplete = true;
+        for (bool letterGuessed : guessed) {
+            if (!letterGuessed) {
+                wordComplete = false;
+                break;
+            }
+        }
+        if (wordComplete) {
+            displayWord(word, guessed);
+            std::cout << "Congrats! You've guessed the word!" << std::endl;
+            break;
+        }
     }
 
-    // Additional code for handling the end of the game (e.g., win or lose)
+    if (wrongGuesses == maxGuesses) {
+        displayHangman(wrongGuesses);
+        std::cout << "Sorry, you've lost. The word was: " << word << std::endl;
+    }
 
     return 0;
 }
